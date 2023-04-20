dbac0726aa6211239a4114d4b0364b121b000874
SpykerNZ <jrspyker@gmail.com>
2023-04-20 19:12:29 +1200

    tidy up readme and scripts

--- merge_sort/sort.js
+++ merge_sort/sort.js
@@ -1,21 +1,20 @@
 function mergeSort(arr) {
   console.log(`Input ${arr}`);
-  // If the array is of length 1, return the array
-  if (arr.length <= 1) return arr;
-  // Otherwise split the array in half and sort both halves!
+  // If the array is less than 2, return the array
+  if (arr.length < 2) return arr;
+  // Otherwise split the array in half
   const half = Math.ceil(arr.length / 2);
   const left = mergeSort(arr.slice(0, half));
-  const right = mergeSort(arr.slice(half));
-  let i = 0;
-  let j = 0;
-  let result = [];
+  const right = mergeSort(arr.slice(half, arr.length));
+  // and sort both halves!
+  const result = [];
+  let i = 0,
+    j = 0;
   while (i + j < arr.length) {
     if (j >= right.length || left[i] < right[j]) {
-      result.push(left[i]);
-      i = i + 1;
+      result.push(left[i++]);
     } else {
-      result.push(right[j]);
-      j = j + 1;
+      result.push(right[j++]);
     }
   }
   return result;
--- merge_sort/sort.test.js
+++ merge_sort/sort.test.js
@@ -6,10 +6,8 @@ describe("sort array", () => {
     { args: [[1, 0]], expected: [0, 1] },
     { args: [[2, 1, 6, 3, 5, 4]], expected: [1, 2, 3, 4, 5, 6] },
     { args: [[7, 6, 5, 4, 3, 2, 1]], expected: [1, 2, 3, 4, 5, 6, 7] },
-    {
-      args: [[8, 2, 4, 6, 7, 5, 3, 1, 9]],
-      expected: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    },
+    { args: [[8, 2, 4, 6, 7, 5, 3, 1]], expected: [1, 2, 3, 4, 5, 6, 7, 8] },
+    { args: [[1, 2, 1, 2]], expected: [1, 1, 2, 2] },
   ];
   testCases.forEach(({ args, expected }) => {
     test(`sort ${args}`, () => {
